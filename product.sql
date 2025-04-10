30f87ea099b98ff3824b78b78fc399d8b9027b8c
jeongmin <jeongmin@naver.com>
2025-04-10 22:04:52 +0900

    5. 제품 테이블/컬럼 영문전환

---
+++ product.sql
@@ -0,0 +1,6 @@
+alter table 제품 rename to product;
+ALTER TABLE product RENAME COLUMN 제품번호 TO productnumber;
+ALTER TABLE product RENAME COLUMN 제품명 TO productname;
+ALTER TABLE product RENAME COLUMN 포장단위 TO packagingunit;
+ALTER TABLE product RENAME COLUMN 단가 TO unitprice;
+ALTER TABLE product RENAME COLUMN 재고 TO inventory;
